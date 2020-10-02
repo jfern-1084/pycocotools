47ebc739faf1d4feee629f2712eba4566cb6ccc4
Johan <johan.27011993@gmail.com>
2020-10-01 21:05:34 -0400

    coco.py accepts dict

--- PythonAPI/pycocotools/coco.py
+++ PythonAPI/pycocotools/coco.py
@@ -79,11 +79,16 @@ class COCO:
         self.dataset,self.anns,self.cats,self.imgs = dict(),dict(),dict(),dict()
         self.imgToAnns, self.catToImgs = defaultdict(list), defaultdict(list)
         if not annotation_file == None:
-            print('loading annotations into memory...')
-            tic = time.time()
-            dataset = json.load(open(annotation_file, 'r'))
-            assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
-            print('Done (t={:0.2f}s)'.format(time.time()- tic))
+            # print('loading annotations into memory...')
+            # tic = time.time()
+            # dataset = json.load(open(annotation_file, 'r'))
+            # assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
+            # print('Done (t={:0.2f}s)'.format(time.time()- tic))
+            # self.dataset = dataset
+            # self.createIndex()
+            
+            dataset = annotation_file
+            assert type(dataset)==dict, 'annotation data format {} not supported'.format(type(dataset))
             self.dataset = dataset
             self.createIndex()
 
